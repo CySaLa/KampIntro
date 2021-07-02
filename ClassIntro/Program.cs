58d1de6574eb3c3010ea02d0eeaf2dd1b2aa5a7d
CySaLa <78034751+CySaLa@users.noreply.github.com>
2021-07-03 00:42:42 +0300

    Add project files.

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ClassIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Kurs kurs1 = new Kurs();
+            kurs1.KursAdi = "C#";
+            kurs1.Egitmen = "Engin Demirog";
+            kurs1.IzlenmeOrani = 68;
+
+
+            Kurs kurs2 = new Kurs();
+            kurs2.KursAdi = "Java";
+            kurs2.Egitmen = "Kerem Varis";
+            kurs2.IzlenmeOrani = 64;
+
+
+            Kurs kurs3 = new Kurs();
+            kurs3.KursAdi = "Python";
+            kurs3.Egitmen = "Berkay Bilgin";
+            kurs3.IzlenmeOrani = 80;
+
+            Kurs kurs4 = new Kurs();
+            kurs4.KursAdi = "C++";
+            kurs4.Egitmen = "Murat Turat";
+            kurs4.IzlenmeOrani = 180;
+
+            Kurs[] kurslar = new Kurs[] { kurs1, kurs2, kurs3, kurs4 };
+
+            foreach (var kurs in kurslar)
+            {
+                Console.WriteLine("Kurs Adi"+":"+kurs.KursAdi + " " +"Izlenme Orani"+":"+ kurs.IzlenmeOrani +" " + "Egitmeni" + ":" + kurs.Egitmen);
+            }
+
+            //Console.WriteLine(kurs1.KursAdi + ":" + kurs1.Egitmen);
+            
+        }
+    }
+
+    class Kurs
+    {
+        public string KursAdi { get; set; }
+        public string Egitmen { get; set; }
+        public int IzlenmeOrani { get; set; }
+
+    }
+}
---
+++ Donguler/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazilimci Kursu";
+            string kurs2 = "Temel Kurs";
+            string kurs3 = "Java";
+
+            //array-dizi
+
+            string[] kurslar = new string[] { "Yazilimci Kursu" , "Temel Kurs", "Java", "Python","JavaScript","C#" };
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+            Console.WriteLine( );
+            Console.WriteLine("---------------------------ForBitti---------------------------");
+            Console.WriteLine();
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+            Console.WriteLine();
+            Console.WriteLine("------------------------SayfaSonu-Footer----------------------");
+        }
+    }
+}
